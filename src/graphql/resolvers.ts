8ae8366eaca785e7bcc69b606129ba0387da5f6c
Gustavo de Camargo <camargo25.gustavo@gmail.com>
2022-03-16 20:48:17 -0300

    Remove pasta test do graphql

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,7 @@
 import { mergeResolvers } from "merge-graphql-schemas";
-import test from "./test/resolver";
 import superHero from "./superHero/resolver";
 
 const resolvers = [
-    test,
     superHero
 ]
 
--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,9 +1,7 @@
 import { mergeTypes } from "merge-graphql-schemas";
-import test from "./test/type";
 import superHero from "./superHero/type";
 
 const types = [
-    test,
     superHero
 ]
 
